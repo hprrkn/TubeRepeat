935bb048a3bc1074cce4550bd64110bdfb2125d0
hprrkn <prrknh@gmail.com>
2015-10-27 00:15:37 +0900

    とりあえず雛形

---
+++ src/youtube_api.js
@@ -0,0 +1,13 @@
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/player_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  var player;
+  function onYouTubePlayerAPIReady() {
+    player = new YT.Player('player', {
+      height: '390',
+      width: '640',
+      videoId: '3_48YF38Cko'
+    });
+  }
